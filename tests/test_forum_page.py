98b64cd6dc24e9d1e522dcc43c838312cb9fc7e8
Aaron <scdxwangtao@gmail.com>
2022-04-20 07:31:38 +0800

    update_2022_04_20

--- tests/test_forum_page.py
+++ tests/test_forum_page.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = "https://v.huya.com/g/all?set_id=51"        # 舞蹈
     # url = "https://v.huya.com/g/all?set_id=65"      # 音乐
     hu_ya = forum_page.HuYaDownload(url)
-    hu_ya.get_video_page_list(first_page=1, count_page=10)
+    hu_ya.get_video_page_list(first_page=51, count_page=1)
 
     print("视频id总数为：{}".format(tools.count_lines("{}/video_id_list_{}.txt".format(hu_ya.temp_path, hu_ya.set_id))))
     hu_ya.get_all_video_url()
